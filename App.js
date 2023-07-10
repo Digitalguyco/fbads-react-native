075714693172b13f0cd5fc2d7e20a30ec13b737e
Digitalguyco <youngkhito@gmail.com>
2023-07-10 19:33:40 +0100

    first push

---
+++ App.js
@@ -0,0 +1,142 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+import { InterstitialAdManager } from 'react-native-fbads';
+
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+// type SectionProps = PropsWithChildren<{
+//   title: string;
+// }>;
+
+function Section({children, title}) {
+  const isDarkMode = useColorScheme() === 'dark';
+  
+
+
+
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+React.useEffect(() => {
+  InterstitialAdManager.loadAd('YOUR_INTERSTITIAL_AD_PLACEMENT_ID')
+    .then(() => {
+      setIsAdLoaded(true);
+    })
+    .catch(error => {
+      // Handle ad loading error
+    });
+}, []);
+
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const [isAdLoaded, setIsAdLoaded] = React.useState(false);
+  const handleButtonClick = () => {
+    if (isAdLoaded) {
+      InterstitialAdManager.showAd('YOUR_INTERSTITIAL_AD_PLACEMENT_ID')
+        .then(didClick => {
+          // Handle ad click event if needed
+        })
+        .catch(error => {
+          // Handle ad loading error
+        });
+    } else {
+      // Ad is not loaded yet, show a message or take other actions
+      console.log('Ad is not loaded yet');
+    }
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text>click here to see ads
+            <Button title="Show Ad" onPress={handleButtonClick} />
+          </Section>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
